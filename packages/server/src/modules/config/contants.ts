fcd8590fe9faff78c0cd18fc4e4a7dfda92e6fe4
evan.feng <evan.feng@temu.com>
2023-09-26 22:12:27 +0800

    feat: 优化pm2日志输出

--- packages/server/src/modules/config/contants.ts
+++ packages/server/src/modules/config/contants.ts
@@ -3,7 +3,10 @@ import path from 'path';
 
 const platform = os.type();
 export const isLinux = platform === 'Linux';
-export const rootDirPath = isLinux ? '/docker-mobile' : path.resolve(os.homedir(), 'docker-mobile');
+export const rootDirPath =
+  process.env.NODE_ENV === 'production'
+    ? '/docker-mobile'
+    : path.resolve(os.homedir(), 'docker-mobile');
 export const configDirPath = path.resolve(rootDirPath, 'config');
 export const userConfigFilePath = path.resolve(rootDirPath, 'config', 'userConfig.yml');
 export const systemConfigFilePath = path.resolve(rootDirPath, 'config', 'systemConfig.yml');
--- pm2.config.js
+++ pm2.config.js
@@ -3,6 +3,15 @@ module.exports = {
     {
       name: 'docker-mobile',
       script: './packages/server/dist/index.cjs',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      out_file: '/docker-mobile/logs/info.log',
+      error_file: '/docker-mobile/logs/error.log',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
     },
   ],
 };
